8ac8a7d299adfd5b31eb923aaa2a43aebaa80403
Git-Harshit <hkg1412@gmail.com>
2024-10-31 23:38:21 +0530

    test: Add test for digits
    Add a test for all of digits conversion by using a 9876543210 conversion
    check as a testcase added under the test_tinytext.py collection
    
    Signed-off-by: Git-Harshit <hkg1412@gmail.com>

--- tests/test_tinytext.py
+++ tests/test_tinytext.py
@@ -11,3 +11,13 @@ def test_something() -> None:
 
     # Assert
     assert tiny == "ᶦᶰᵗᵒ ᵗᶦᶰᶦᵉʳ ᵗᵉˣᵗ"
+
+def test_for_digits() -> None:
+    # Arrange
+    numbers = ''.join(str(character) for character in range(9, -1, -1))      # numbers = 9876543210
+
+    # Act
+    tiny: str = tinytext.tinytext(numbers)
+
+    # Assert
+    assert tiny == "⁹⁸⁷⁶⁵⁴³²¹⁰"
